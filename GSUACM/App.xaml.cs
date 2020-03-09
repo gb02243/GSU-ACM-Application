62dd5db8b25366e0a07c877b3b7acac81c8026c6
griffin <gb02243@georgiasouthern.edu>
2020-03-09 15:06:03 -0400

    New redesign to use MasterDetailPage MVVM pattern

---
+++ GSUACM/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using GSUACM.Services;
+using GSUACM.Views;
+
+namespace GSUACM
+{
+    public partial class App : Application
+    {
+        public bool isLoggedIn;
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+
+            //isLoggedIn = false;
+
+            //if (isLoggedIn)
+            //{
+            //    MainPage = new MainPage();
+            //}
+            //else
+            //{
+            //    MainPage = new WelcomePage();
+            //}
+
+            MainPage = new SignupPage();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
