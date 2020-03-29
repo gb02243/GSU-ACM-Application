cdb920b52f5b1ebeac0051eb87fb535d421bdfa5
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-29 19:23:40 -0400

    ~ minor changes

--- GSUACM/Services/MockChatData.cs
+++ GSUACM/Services/MockChatData.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using GSUACM.Models.ChatModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GSUACM.Services
 {
-    class MockChatData
+    public class MockChatData
     {
+        public static IEnumerable<Chat> Get()
+        {
+            return new List<Chat>
+            {
+                new Chat(){ chat_id="1" },
+                new Chat(){ chat_id="2" },
+                new Chat(){ chat_id="3" },
+                new Chat(){ chat_id="4" },
+                new Chat(){ chat_id="5" },
+                new Chat(){ chat_id="6" },
+                new Chat(){ chat_id="7" },
+                new Chat(){ chat_id="8" },
+                new Chat(){ chat_id="9" },
+                new Chat(){ chat_id="10" },
+                new Chat(){ chat_id="11" },
+                new Chat(){ chat_id="12" }
+            };
+        }
     }
 }
