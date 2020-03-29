5c6b698ffd5de5a712b0cd7699b15093e8b8c0f5
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-29 16:33:37 -0400

    + Implemented ChatListViewModel

--- GSUACM/ViewModels/ChatViewModels/ChatListViewModel.cs
+++ GSUACM/ViewModels/ChatViewModels/ChatListViewModel.cs
@@ -1,18 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Windows.Input;
 using GSUACM.Models.ChatModels;
 using Xamarin.Forms;
 
 namespace GSUACM.ViewModels.ChatViewModels
 {
-    public class ChatListViewModel
+    public class ChatListViewModel : INotifyPropertyChanged
     {
-        
+        public ObservableCollection<Chat> Chats { get; set; } = new ObservableCollection<Chat>();
+
+        public ChatListViewModel()
+        {
+            Chats.Add(new Chat() { chat_id = "1" });
+            Chats.Add(new Chat() { chat_id = "2" });
+            Chats.Add(new Chat() { chat_id = "3" });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
