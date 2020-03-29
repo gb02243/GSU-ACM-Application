f1d5ba2bf877db1f0377b4beee7c196de6c8890c
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-29 16:46:32 -0400

    + created ChatPartialSelector

---
+++ GSUACM/Helpers/ChatPartialSelector.cs
@@ -0,0 +1,28 @@
+﻿using GSUACM.Models.ChatModels;
+using GSUACM.Views.Chat_Partials;
+using Xamarin.Forms;
+
+namespace GSUACM.Helpers
+{
+    class ChatPartialSelector : DataTemplateSelector
+    {
+        DataTemplate directDataTemplate;
+        DataTemplate groupDataTemplate;
+
+        public ChatPartialSelector()
+        {
+            this.directDataTemplate = new DataTemplate(typeof(DirectViewCell));
+            this.groupDataTemplate = new DataTemplate(typeof(GroupViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var messageType = item as Chat;
+            if (messageType == null)
+                return null;
+
+
+            return (messageType.IsGroup) ? groupDataTemplate : directDataTemplate;
+        }
+    }
+}
