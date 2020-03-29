93453506be3d7dae5d09c88f851f9a6c64998141
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-29 15:42:56 -0400

    ~ Fixed Directory Formatting

--- GSUACM/ViewModels/ChatViewModels/ChatListViewModel.cs
+++ GSUACM/ViewModels/ChatViewModels/ChatListViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GSUACM.Models.Chat;
 
 namespace GSUACM.ViewModels.Chat
 {
     class ChatListViewModel
     {
+        IList<Chat> source;
     }
 }
