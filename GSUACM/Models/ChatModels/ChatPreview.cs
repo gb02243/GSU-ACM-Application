a9367a079575f78154f36a2cfb9755166dd41a19
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-29 22:17:49 -0400

    ~ renamed ChatInfo to ChatPreview

--- GSUACM/Models/ChatModels/ChatPreview.cs
+++ GSUACM/Models/ChatModels/ChatPreview.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace GSUACM.Models.ChatModels
 {
-    public class ChatInfo
+    public class ChatPreview
     {
         public string Chat_ID { get; set; }
         public string F_name { get; set; }
         public string L_name { get; set; }
         public string FullName => F_name + " " + L_name;
+        public string Msg_Preview { get; set; }
     }
 }
