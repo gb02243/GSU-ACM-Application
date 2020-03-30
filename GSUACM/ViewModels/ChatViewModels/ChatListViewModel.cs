4a35fab00ab1f64d7241cdd81019267d6d5ea53c
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-29 22:06:13 -0400

    + implemented ChatListViewModel

--- GSUACM/ViewModels/ChatViewModels/ChatListViewModel.cs
+++ GSUACM/ViewModels/ChatViewModels/ChatListViewModel.cs
@@ -1,17 +1,37 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows.Input;
 using GSUACM.Models.ChatModels;
+using GSUACM.Services;
 using Xamarin.Forms;
 
 namespace GSUACM.ViewModels.ChatViewModels
 {
-    public class ChatListViewModel : BaseViewModel
-    {        
+    public class ChatListViewModel : INotifyPropertyChanged
+    {
+        public List<Chat> Chats { get; set; }
+        public ICommand DeleteCommand { get; private set; }
         public ChatListViewModel()
         {
+            // TODO: get chat data from database
+            MockChatData.Fill(5);
+            Chats = new List<Chat>(MockChatData.Get());
 
+            DeleteCommand = new Command<string>(DeleteChat);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void DeleteChat(string chat_id)
+        {
+            MockChatData.Remove(chat_id);
+            UpdateList();
+            PropertyChanged(this, new PropertyChangedEventArgs("Chats"));
+        }
+        private void UpdateList()
+        {
+            Chats = new List<Chat>(MockChatData.Get());
         }
     }
 }
