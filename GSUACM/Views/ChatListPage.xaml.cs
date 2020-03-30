69751ce7f6a7393eadd3f7ea130256321ffca4c9
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-29 22:08:01 -0400

    ~ removed all methods and created binding context for viewmodel instead

--- GSUACM/Views/ChatListPage.xaml.cs
+++ GSUACM/Views/ChatListPage.xaml.cs
@@ -1,12 +1,4 @@
-﻿using GSUACM.Models.ChatModels;
-using GSUACM.Services;
-using GSUACM.ViewModels.ChatViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using GSUACM.ViewModels.ChatViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,12 +7,10 @@ namespace GSUACM.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChatListPage : ContentPage
     {
-        public List<Chat> Chats { get; set; }
         public ChatListPage()
         {
             InitializeComponent();
-            Chats = new List<Chat>(MockChatData.Get());
-            chatsList.ItemsSource = Chats;
+            BindingContext = new ChatListViewModel();
         }
     }
 }
