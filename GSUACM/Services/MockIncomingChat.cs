aaa8dede808411dc752b0b78f109fbf5fdedc068
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-29 22:25:10 -0400

    + created class to mock an incoming direct message

---
+++ GSUACM/Services/MockIncomingChat.cs
@@ -0,0 +1,26 @@
+﻿using GSUACM.Models.ChatModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GSUACM.Services
+{
+    class MockIncomingChat
+    {
+        static List<ChatPreview> ChatList;
+
+        public static void MockIncomingMessage(int messages, int people)
+        {
+            if(messages > 0 && people > 0)
+            {
+                for (int i = 0; i < messages; i++)
+                {
+                    for (int j = 0; j < people; j++)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
