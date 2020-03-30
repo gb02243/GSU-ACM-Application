6e66796d78927e2917dd1c0a0d129ec1e04313ec
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-30 00:01:13 -0400

    + added chat navigation

--- GSUACM/ViewModels/ChatViewModels/ChatListViewModel.cs
+++ GSUACM/ViewModels/ChatViewModels/ChatListViewModel.cs
@@ -1,48 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 using GSUACM.Models.ChatModels;
 using GSUACM.Services;
+using GSUACM.Views.Chat;
 using Xamarin.Forms;
 
 namespace GSUACM.ViewModels.ChatViewModels
 {
     public class ChatListViewModel : INotifyPropertyChanged
     {
-        //public List<Chat> Chats { get; set; }
-        //public ICommand DeleteCommand { get; private set; }
-        //public ChatListViewModel()
-        //{
-        //    // TODO: get chat data from database
-        //    MockChatData.Fill(5);
-        //    Chats = new List<Chat>(MockChatData.Get());
-
-        //    DeleteCommand = new Command<string>(DeleteChat);
-        //}
-
-        //public event PropertyChangedEventHandler PropertyChanged;
-
-        //public void DeleteChat(string chat_id)
-        //{
-        //    MockChatData.Remove(chat_id);
-        //    UpdateList();
-        //    PropertyChanged(this, new PropertyChangedEventArgs("Chats"));
-        //}
-        //private void UpdateList()
-        //{
-        //    Chats = new List<Chat>(MockChatData.Get());
-        //}
-
+        public INavigation Navigation { get; set; }
         public List<ChatPreview> Chats { get; set; }
         public ICommand DeleteCommand { get; private set; }
-        public ChatListViewModel()
+        public ICommand OpenChatCommand { get; private set; }
+        public ChatListViewModel(INavigation navigation)
         {
+            this.Navigation = navigation;
             // TODO: get chat data from database
             MockIncomingChat.MockIncomingMessage(5, 5);
             Chats = new List<ChatPreview>(MockIncomingChat.Get());
 
             DeleteCommand = new Command<string>(DeleteChat);
+            OpenChatCommand = new Command<string>(OpenChat);
         }
         public void DeleteChat(string chat_id)
         {
@@ -53,6 +35,10 @@ namespace GSUACM.ViewModels.ChatViewModels
         private void UpdateList()
         {
             Chats = new List<ChatPreview>(MockIncomingChat.Get());
+        }
+        public async void OpenChat(string chat_id)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new ChatPage()));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
