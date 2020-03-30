e45b26b08313d18dd8853741be1db6841a01ad40
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-03-30 00:21:32 -0400

    + implemented ChatPageViewModel

--- GSUACM/ViewModels/ChatViewModels/ChatPageViewModel.cs
+++ GSUACM/ViewModels/ChatViewModels/ChatPageViewModel.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace GSUACM.ViewModels.ChatViewModels
 {
     class ChatPageViewModel
     {
+        public INavigation Navigation { get; set; }
+        public string FullName { get; set; }
+        public ICommand BackCommand { get; private set; }
+        public string Chat_ID { get; private set; }
+        public ChatPageViewModel(INavigation navigation, string chat_id)
+        {
+            this.Navigation = navigation;
+
+            BackCommand = new Command(CloseModal);
+
+            this.Chat_ID = chat_id;
+
+            // TODO: Connect to database
+            FullName = "Person "+Chat_ID;
+        }
+
+        public void CloseModal()
+        {
+            Navigation.PopModalAsync();
+        }
     }
 }
