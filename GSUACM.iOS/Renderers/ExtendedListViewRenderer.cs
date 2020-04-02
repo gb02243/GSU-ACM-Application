b1bcfb9c850c0481557523e4c9da8c6342df8404
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-04-02 07:54:54 -0400

    + added custom renderer

---
+++ GSUACM.iOS/Renderers/ExtendedListViewRenderer.cs
@@ -0,0 +1,26 @@
+﻿using GSUACM.Controls;
+using GSUACM.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedListView), typeof(ExtendedListViewRenderer))]
+namespace GSUACM.iOS.Renderers
+{
+    class ExtendedListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control != null)
+                {
+                    Control.AllowsSelection = false;
+                    Control.AlwaysBounceVertical = false;
+                    Control.Bounces = false;
+                }
+            }
+        }
+    }
+}
