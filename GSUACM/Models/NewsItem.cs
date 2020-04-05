c8c0b7e926e0ac6c4e0fd3af554adae7a9f36686
Griffin Bryant <gb02243@georgiasouthern.edu>
2020-04-05 11:24:13 -0400

    + created news item

---
+++ GSUACM/Models/NewsItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GSUACM.Models
+{
+    public class NewsItem
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Body { get; set; }
+        // TODO: test with a string shorter than 120
+        public string BodyPreview => Body.Substring(0, 120)+"...";
+    }
+}
