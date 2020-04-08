d45c5b8d4b441fc19951d21145b8d5bc2fe02863
Everrick Wright <ew02726@georgiasouthern.edu>
2020-04-08 12:46:53 -0400

    editProfilepage

---
+++ GSUACM/Views/editProfile.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GSUACM.Views
+{
+    public partial class editProfile : ContentPage
+    {
+        public editProfile()
+        {
+            InitializeComponent();
+        }
+        private void labelGoToLogin_Click(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpdateProfile_Click(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void changePasswowrd_Click(Object sender, EventArgs e)
+        {
+
+        }
+    }
+}
