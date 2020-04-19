6acf2596120e1ad83726f35e6b548810cac7b81d
Everrick Wright <ew02726@georgiasouthern.edu>
2020-04-18 23:14:40 -0400

    searchPage

---
+++ GSUACM/Views/searchPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using GSUACM.ViewModels.ControlPanel;
+using Xamarin.Forms;
+
+namespace GSUACM.Views
+{
+    public partial class searchPage : ContentPage
+    {
+        public searchPage()
+        {
+            InitializeComponent();
+            BindingContext = new TitlesPanelViewModel(this.Navigation);
+        }
+    }
+}
